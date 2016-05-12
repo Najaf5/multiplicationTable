c421b1c320886fe8fbbf55c32739df29a7515565
Jack Schiffman <jschiffman2017@pingry.org>
2016-05-12 14:47:02 -0400

    Final commit.

---
+++ multiplicationTable.py
@@ -0,0 +1,29 @@
+def multiplicationTable(smaller, larger):
+	''' Returns a multiplication table containing all the products between the smaller and larger numbers.  '''
+	
+	table = []
+	output = "  "
+	
+	for j in range(smaller, larger + 1):
+		row = [str(j) + "|"]
+		output += "   " + str(j)
+	
+		for i in range(smaller, larger + 1):
+			currentProduct = i * j
+			row.append(currentProduct)
+			
+		table.append(row)
+	output += "\n   ---------------\n"
+	
+	for item in table:
+		for z in item:
+			output += " {} ".format(z)
+		output += "\n"
+	
+	return(output)
+	
+#----------MAIN PROGRAM----------
+
+smaller = int(input("Tell me a starting number: "))
+larger = int(input("Tell me an ending number: "))
+print(multiplicationTable(smaller, larger))
