d3edf6bd0b5ec68277b05fd0ba21119b2bf9b179
Najaf5 <71011993+Najaf5@users.noreply.github.com>
2020-10-02 10:40:11 +0530

    Update multiplicationTable.py

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,29 +1,17 @@
-def multiplicationTable(smaller, larger):
-	''' Returns a multiplication table containing all the products between the smaller and larger numbers inputed by the user.  '''
-	
-	table = []
-	output = "  "
-	
-	for j in range(smaller, larger + 1):
-		row = [str(j) + "|"]
-		output += "   " + str(j)
-	
-		for i in range(smaller, larger + 1):
-			currentProduct = i * j
-			row.append(currentProduct)
-			
-		table.append(row)
-	output += "\n   ---------------\n"
-	
-	for item in table:
-		for z in item:
-			output += " {} ".format(z)
-		output += "\n"
-	
-	return(output)
-	
-#----------MAIN PROGRAM----------
 
-smaller = int(input("Tell me a starting number: "))
-larger = int(input("Tell me an ending number: "))
-print(multiplicationTable(smaller, larger))
+i = 1
+while i < 10:
+    j = 1
+    while j < 10:
+        print("%4d" % (i * j), end="")
+        j += 1
+    print()
+    i += 1
+
+
+
+	
+	
+
+	
+
